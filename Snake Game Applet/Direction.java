b73e3897a8c2604dae9117baa16c3d5a847a905c
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-19 06:11:20 +0100

    Create Direction.java

---
+++ Snake Game Applet/Direction.java
@@ -0,0 +1,8 @@
+
+public class Direction {
+	public static final int NO_DIRECTION = 0;
+    public static final int NORTH = 1;
+    public static final int SOUTH = 2;
+    public static final int WEST = 3;
+    public static final int EAST = 4;
+}
