a53ca882f2289b638e66d49349d7a5d86c3c7a17
Dennis Mubamba <dennis.mubamba@mikaexpress.com>
2019-10-12 13:04:09 +0200

    Added Otp generator

---
+++ OtpGenerator.java
@@ -0,0 +1,29 @@
+import OTP.Permutations;
+import Redis.RedisHandler;
+import org.apache.commons.lang.RandomStringUtils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class OtpGenerator {
+
+    public static void main(String[] args) {
+
+        String numbers = RandomStringUtils.randomNumeric(10);
+
+        Random rand = new Random();
+        int length = 6;
+
+        char[] otp = new char[length];
+
+        for (int i = 0; i < length; i++) {
+
+            otp[i] = numbers.charAt(rand.nextInt(numbers.length()));
+
+        }
+
+        System.out.println(otp);
+    }
+}
