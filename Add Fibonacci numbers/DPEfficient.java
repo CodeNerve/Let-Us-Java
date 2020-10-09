51b97300ee03ded00fb9952546bec07ad576c535
mKittyD <51280288+mKittyD@users.noreply.github.com>
2020-10-09 08:59:44 +0200

    Create DPEfficient.java

---
+++ Add Fibonacci numbers/DPEfficient.java
@@ -0,0 +1,17 @@
+class Solution {
+    Map<Integer,Integer> map = new HashMap<>();
+    
+    public int fib(int n) {
+        if(n <= 0) { 
+            return 0;
+        } else if(n == 1 || n == 2) {
+            return 1;
+        } else if(map.containsKey(n)){
+            return map.get(n);
+        } else {
+            int answer = fib(n - 1) + fib(n - 2);
+            map.put(n, answer);
+            return answer;
+        }
+    }
+}
