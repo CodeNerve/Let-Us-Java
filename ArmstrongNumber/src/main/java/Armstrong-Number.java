9c8703b2e4881c432caea9983618c5596780ad51
Fazreil <fazreil@gmail.com>
2020-10-11 01:21:36 +0800

    refactor to add public classes into Armstrong-Number

--- ArmstrongNumber/src/main/java/Armstrong-Number.java
+++ ArmstrongNumber/src/main/java/Armstrong-Number.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-class ArmstrongNum {
+public class ArmstrongNum {
 
 	/* driver function commented
   public static void main(String[] args) {
-		
+
 		Scanner kb = new Scanner(System.in);
 		int N = kb.nextInt();
-		
+
 		int k = length(N);
-		
+
 		System.out.println(isArmstrongNumber(N, k));
-		
+
 		kb.close();
 	}
   */
@@ -30,16 +30,16 @@ class ArmstrongNum {
 	public static boolean isArmstrongNumber(int N, int k) {
 		long powerSum = 0;
 		int n = N;
-		
+
 		while (n != 0) {
 			int rem = n % 10;
 
 			powerSum += (int) Math.pow(rem, k);
-			
+
 			n /= 10;
 		}
 		System.out.println(powerSum);
 		return (powerSum==N);
 	}
-  
+
 }
