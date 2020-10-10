daf872eac23064fa5ab6b7367d4aaff8cc475b84
Fazreil <fazreil@gmail.com>
2020-10-11 01:37:09 +0800

    refactor to add public classes into Codechef, renamed from FANCY to adhere to java convention

--- Fancy/src/main/java/Codechef.java
+++ Fancy/src/main/java/Codechef.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+public class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
