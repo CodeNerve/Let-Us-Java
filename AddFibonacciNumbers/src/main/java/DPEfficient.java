c5dfe8b1b264685d32e5eb332e1c880b4cf7bc3f
Fazreil <fazreil@gmail.com>
2020-10-11 19:27:41 +0800

    declare Map and HashMap for class DPEfficient.java

--- AddFibonacciNumbers/src/main/java/DPEfficient.java
+++ AddFibonacciNumbers/src/main/java/DPEfficient.java
@@ -1,8 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     Map<Integer,Integer> map = new HashMap<>();
-    
+
     public int fib(int n) {
-        if(n <= 0) { 
+        if(n <= 0) {
             return 0;
         } else if(n == 1 || n == 2) {
             return 1;
