b81c5dc22a8ff209ebdd6b4ed66128814e090b2d
diksharai9 <btbtc20297_diksha@banasthali.in>
2021-10-06 22:48:42 +0530

    two new solutions

---
+++ LeetCodeSolutions/src/main/java/FindPeakInMountArray.java
@@ -0,0 +1,22 @@
+public class FindPeakInMountArray {
+    public static void main(String[] args) {
+       // Leetcode default...............code
+
+    }
+
+
+        public int peakIndexInMountainArray(int[] arr) {
+            int start= 0;
+            int end =arr.length-1;
+
+            while(start<end){
+                int mid = start +((end-start)/2) ;
+                if(arr[mid]>arr[mid+1]){
+                    end = mid;
+                }else {
+                    start= mid+1;
+                }
+            }
+            return start;
+    }
+}
