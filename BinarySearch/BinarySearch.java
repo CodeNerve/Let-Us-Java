816f26cec1ae9bf58042d225a8f5d3bcbf880dc2
Amrita Kesh <74363370+amritakesh@users.noreply.github.com>
2021-10-21 18:26:39 +0530

    Update BinarySearch.java

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -1,43 +1,35 @@
 package com.company;
 
-import java.util.Scanner;
 
-public class BinSearchDesc
-{
-    public static void main(String[] args)
-    {
-        int[] arr = {6, 5, 4, 3, 2, 1};
-        int target = 2;
+public class BinarySearch {
 
-        int res = binsearch(arr, target);
-      
-        System.out.println(res);
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
     }
 
-    private static int binsearch(int[] arr, int target)
-    {
+    // return the index
+    // return -1 if it does not exist
+    static int binarySearch(int[] arr, int target) {
         int start = 0;
-        int end = arr.length-1;
+        int end = arr.length - 1;
 
-        while(start <= end){
-            int middle = start + (end - start) / 2;
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; --> might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
 
-            if(target < arr[middle])
-            {
-                end = middle - 1;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
             }
-            else if(target > arr[middle])
-            {
-                start = middle + 1;
-            }
-            else
-            {
-                return middle;
-            }
-
         }
         return -1;
     }
-
-
 }
