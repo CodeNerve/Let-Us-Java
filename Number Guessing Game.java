214779eca1cfae3e7dbefd1d894c24a9b00c5b2a
Aman Mishra <72151791+AmanMishra04@users.noreply.github.com>
2022-10-03 15:21:07 +0530

    Create  Number Guessing Game.java

---
+++ Number Guessing Game.java
@@ -0,0 +1,73 @@
+package guessinggame;
+
+* Java game “Guess a Number” that allows user to guess a random number that has been generated.
+
+*/
+
+import javax.swing.*;
+
+publicclassGuessingGame {
+
+publicstaticvoidmain(String[] args) {
+
+int computerNumber = (int) (Math.random()*100 + 1);
+
+int userAnswer = 0;
+
+System.out.println("The correct guess would be " + computerNumber);
+
+int count = 1;
+
+while (userAnswer != computerNumber)
+
+{
+
+String response = JOptionPane.showInputDialog(null,
+
+"Enter a guess between 1 and 100", "Guessing Game", 3);
+
+userAnswer = Integer.parseInt(response);
+
+JOptionPane.showMessageDialog(null, ""+ determineGuess(userAnswer, computerNumber, count));
+
+count++;
+
+}
+
+}
+
+publicstatic String determineGuess(int userAnswer, int computerNumber, int count){
+
+if (userAnswer <=0 || userAnswer >100) {
+
+return"Your guess is invalid";
+
+}
+
+elseif (userAnswer == computerNumber ){
+
+return"Correct!\nTotal Guesses: " + count;
+
+}
+
+elseif (userAnswer > computerNumber) {
+
+return"Your guess is too high, try again.\nTry Number: " + count;
+
+}
+
+elseif (userAnswer < computerNumber) {
+
+return"Your guess is too low, try again.\nTry Number: " + count;
+
+}
+
+else {
+
+return"Your guess is incorrect\nTry Number: " + count;
+
+}
+
+}
+
+}
