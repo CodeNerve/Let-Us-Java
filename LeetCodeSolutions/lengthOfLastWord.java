f341026cf2cfcddc05211c6da9af8559ec87aece
kushagra sharma <2001kushagrasharma@gmail.com>
2022-10-09 17:18:25 +0530

    lengthOfLastWord

---
+++ LeetCodeSolutions/lengthOfLastWord.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else {  
+                if (length > 0) return length;
+            }
+        }
+        return length;
+    }
+}
