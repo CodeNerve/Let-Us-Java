27574216c996fdf2b3ac1490db4c8c858d698f9b
ritik maheshwari <ritikdargar@gmail.com>
2022-10-11 22:20:57 +0530

    CharArrayToString

---
+++ CharArrayToString/Main.java
@@ -0,0 +1,9 @@
+class CharArrayToString
+{
+   public static void main(String args[])
+   {
+      char[] ch = {'g', 'o', 'o', 'd', ' ', 'n', 'i', 'g', 'h', 't'};
+      String str = new String(ch);
+      System.out.println(str);
+   }
+}
