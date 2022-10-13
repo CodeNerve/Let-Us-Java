f336274ae86621d19240fed18c2bf89a16ac07fd
Sharduli Pandey <shardulipandey8090@gmail.com>
2022-10-13 10:49:18 +0530

    Added Snake Game with various Features and GUI

---
+++ Snake Game with Various Features and GUI/GameFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+//GAME FRAME CLASS EXTENDING JFRAME CLASS
+public class GameFrame extends JFrame {
+
+    //CONSTRUCTOR
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
