5d2ea725037e8d75ee7220934b7122639524b9f1
UdbhavJain <udbhavjain35@gmail.com>
2022-10-16 02:06:00 +0530

    atm code

--- atm/atm.java
+++ atm/atm.java
@@ -1,117 +1,59 @@
-import java.util.Scanner;   
-
-public class Atm 
-
-{   
-
-     public static void main(String args[] )   
-
-    {   
-
-              int balance = 100000, withdraw, deposit;   
-
-           
-
-            Scanner sc = new Scanner(System.in);   
-
-           
-
-        while(true)   
-
-        {   
-
-            System.out.println("Automated Teller Machine");   
-
-            System.out.println("Choose 1 for Withdraw");   
-
-            System.out.println("Choose 2 for Deposit");   
-
-            System.out.println("Choose 3 for Check Balance");   
-
-            System.out.println("Choose 4 for EXIT");   
-
-            System.out.print("Choose the operation you want to perform:");   
-
-               
-
-                     int choice = sc.nextInt();   
-
-            switch(choice)   
-
-            {   
-
-                case 1:   
-
-        System.out.print("Enter money to be withdrawn:");   
-
-                       
-
-              withdraw = sc.nextInt();            
-
-        if(balance >= withdraw)   
-
-        {   
-
-                   balance = balance - withdraw;   
-
-            System.out.println("Please collect your money");   
-
-        }   
-
-        else   
-
-        {   
-
-                      System.out.println("Insufficient Balance");   
-
-        }   
-
-        System.out.println("");   
-
-        break;   
-
-    
-
-                case 2:   
-
-                       
-
-        System.out.print("Enter money to be deposited:");   
-
-                       
-
-             deposit = sc.nextInt();   
-
-                       
-
-               balance = balance + deposit;   
-
-        System.out.println("Your Money has been successfully depsited");   
-
-        System.out.println("");   
-
-        break;   
-
-    
-
-                case 3:    
-
-        System.out.println("Balance : "+balance);   
-
-        System.out.println("");   
-
-        break;   
-
-    
-
-                case 4:   
-
-        System.exit(0);   
-
-            }   
-
-        }   
-
-    }   
-
-}   
+20.	import java.util.Scanner;  
+21.	public class Atm
+22.	{  
+23.	     public static void main(String args[] )  
+24.	    {  
+25.	              int balance = 100000, withdraw, deposit;  
+26.	          
+27.	            Scanner sc = new Scanner(System.in);  
+28.	          
+29.	        while(true)  
+30.	        {  
+31.	            System.out.println("Automated Teller Machine");  
+32.	            System.out.println("Choose 1 for Withdraw");  
+33.	            System.out.println("Choose 2 for Deposit");  
+34.	            System.out.println("Choose 3 for Check Balance");  
+35.	            System.out.println("Choose 4 for EXIT");  
+36.	            System.out.print("Choose the operation you want to perform:");  
+37.	              
+38.	                     int choice = sc.nextInt();  
+39.	            switch(choice)  
+40.	            {  
+41.	                case 1:  
+42.	        System.out.print("Enter money to be withdrawn:");  
+43.	                      
+44.	              withdraw = sc.nextInt();           
+45.	        if(balance >= withdraw)  
+46.	        {  
+47.	                   balance = balance - withdraw;  
+48.	            System.out.println("Please collect your money");  
+49.	        }  
+50.	        else  
+51.	        {  
+52.	                      System.out.println("Insufficient Balance");  
+53.	        }  
+54.	        System.out.println("");  
+55.	        break;  
+56.	   
+57.	                case 2:  
+58.	                      
+59.	        System.out.print("Enter money to be deposited:");  
+60.	                      
+61.	             deposit = sc.nextInt();  
+62.	                      
+63.	               balance = balance + deposit;  
+64.	        System.out.println("Your Money has been successfully depsited");  
+65.	        System.out.println("");  
+66.	        break;  
+67.	   
+68.	                case 3:   
+69.	        System.out.println("Balance : "+balance);  
+70.	        System.out.println("");  
+71.	        break;  
+72.	   
+73.	                case 4:  
+74.	        System.exit(0);  
+75.	            }  
+76.	        }  
+77.	    }  
+78.	}  
