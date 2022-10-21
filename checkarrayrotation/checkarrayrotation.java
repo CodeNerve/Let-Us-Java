4d05c669095095513ef99ffdbab71dd420bac41c
Alok kumar <75192701+alok007kr@users.noreply.github.com>
2022-10-21 23:22:06 +0530

    Create checkarrayrotation.java
    I have added code to check array rotation in array.

---
+++ checkarrayrotation/checkarrayrotation.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+        
+        String[] strNums; 
+        strNums = br.readLine().split("\\s");
+        
+
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while(t > 0) {
+
+            int[] input = takeInput();
+            int ans = Solution.arrayRotateCheck(input);
+            System.out.println(ans);
+
+            t -= 1;
+        }
+    }
+}
+
+
+class Solution {
+public static int arrayRotateCheck(int[] arr){
+for(int i = 0; i < arr.length - 1; i++){
+if(arr[i] > arr[i+1])
+return (i+1);
+}
+return 0;
+}
+}
