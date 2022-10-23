a6cb0ac411dd78a129863c02a5a3420975be235c
Adityagupta200 <aditya2004g@gmail.com>
2022-10-23 10:28:00 +0530

    Added code for finding subsets of n natural numbers.

---
+++ Recursion/Subsets_of_n_natural_numbers.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class all_subsets_of_first_n_natural_numbers {
+    public static void printSubsets(ArrayList<Integer> subset) {
+        for(int i=0; i<subset.size(); i++) {
+            System.out.print(subset.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n, ArrayList<Integer> subset) {
+        if(n == 0) {
+            printSubsets(subset);
+            return;
+        }
+        findSubsets(n-1, subset);
+        subset.add(n);
+        findSubsets(n-1, subset);
+        subset.remove(subset.size() - 1);
+    }
+}
