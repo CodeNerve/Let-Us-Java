719c891d8c4f60288a9b87b79cf1d6d9e28d2a7c
ADARSH <98254294+ADARSH-863@users.noreply.github.com>
2022-10-24 17:48:19 +0530

    Create NumberGuessingGame.java
    This is a number guessing game written in java language. In this game the computer randomly chooses a number and the user has to guess it. If the number is exactly same to the number generated by the computer then the user wins the game else the computer give the hint that weather the actual number is smaller or lager than the guessed number.
    
    Please don't forget to change the class name from Main if it throws error.

---
+++ NumberGuessingGame.java
@@ -0,0 +1,78 @@
+
+// Author: ADARSH
+// https://github.com/ADARSH-863
+
+import java.util.*;
+
+class random
+{
+
+    private int n;
+
+    public int ran()
+    {
+        Random num = new Random();
+        return num.nextInt(10);
+    }
+
+    public int takeUserInput()
+    {
+        Scanner in = new Scanner(System.in);
+        int i= in.nextInt();
+        return i;
+    }
+
+    public int setCounter()
+    {
+        return n=0;
+    }
+
+    public int setter_numberOfGuesses(int g)
+    {
+        return n=++g;
+    }
+
+    public int getter_numberOfGuesses()
+    {
+        return n;
+    }
+
+    public boolean isCorrectNumber(int uno,int rno,int gno)
+    {
+        if(uno==rno)
+        {
+            System.out.printf((gno==1)?"\nWohoo!!!\nYou nailed it in your first shot.\n\n":"\nCongratulations! \nYou guessed the corrrect number in %d attempts.\n\n",gno);
+            return true;
+        }
+        else
+        {
+            System.out.println((uno>rno)?"\nYour number is larger than the actual number\nTry Again!":"\nYour number is smaller than the actual number\nTry Again!");
+            System.out.println();
+            return false;
+        }
+    }
+
+}
+
+// Please edit the class name if it is throwing error.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nEnter a single-digit number:");
+        random r = new random();
+        int gno=r.setCounter(),uno;
+        int rno=r.ran();
+        // //cheat
+        // System.out.println("The actual number choosen by computer is "+rno);
+        // // A gift from ADARSH.
+
+
+        do {
+            uno=r.takeUserInput();
+            gno=r.setter_numberOfGuesses(gno);
+            gno=r.getter_numberOfGuesses();
+
+        }
+        while(!(r.isCorrectNumber(uno,rno,gno)));
+
+    }
+}
