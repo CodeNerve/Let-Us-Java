c8fdfbf9652b0cb538f4afbd082f52b902b5b1fb
sparsh kumar <71932955+1234spa@users.noreply.github.com>
2022-10-31 13:20:04 +0530

    created infinite loop.java

---
+++ Algorithms/infinite loop.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+import java.lang.*;  
+class Rextester  
+{    
+    public static void main(String args[])  
+    {  
+        do  
+        {  
+         System.out.print("javaTpoint");  
+         System.out.print(" ");  
+        }while(true);  
+    }  
+}  
